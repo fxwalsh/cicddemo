bf5b4e5581ad6308cb944ff330b86e87373bed2a
fxwalsh <fxwalsh@wit.ie>
2017-04-23 14:44:10 +0100

    updated api

--- src/api.js
+++ src/api.js
@@ -1,21 +1,22 @@
 import axios from 'axios';
+import config from './../config';
 
 export const upvote = postId => {
-  return axios.post(`/api/posts/${postId}/upvote`)
+  return axios.post(`${config.serverUrl}/api/posts/${postId}/upvote`)
               .then(resp => resp.data);
 };
 
 export const getAll = () => {
-   return axios('/api/posts')
+   return axios(`${config.serverUrl}/api/posts`)
               .then(resp => resp.data);
 };
 
 export const getPost = postId => {
-  return axios.get(`/api/posts/${postId}`)
+  return axios.get(`${config.serverUrl}/api/posts/${postId}`)
               .then(resp => resp.data);
 };
 
 export const add = (newTitle, newLink) => {
-  return axios.post('/api/posts', { title: newTitle, link: newLink })
+  return axios.post(`${config.serverUrl}/api/posts`, { title: newTitle, link: newLink })
               .then(resp => resp.data);
 };
